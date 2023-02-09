e8633b34f99e594ff68f7ecc855447fc27f619af
minhthuan191 <minhthuan1912001@gmail.com>
2023-02-09 23:08:43 +0700

    Change connection string

--- Utils/DBContext.cs
+++ Utils/DBContext.cs
@@ -28,7 +28,7 @@ namespace Tutoree.Utils
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=Tutoree;User ID=sa;Password=1234567890;MultipleActiveResultSets=true;Integrated Security=false;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("Data Source=13.212.125.242,1433;Initial Catalog=Tutoree;User ID=sa;Password=1234567890Aa;MultipleActiveResultSets=true;Integrated Security=false;TrustServerCertificate=true");
         }
 
 
