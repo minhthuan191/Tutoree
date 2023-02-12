1d1d40b4f270c8a3228b012dc5140ab883b52e64
Tai Tang <tommytaitang@gmail.com>
2023-02-13 06:50:04 +0700

    [Tai] Create Login Page

--- Utils/Common/Router.cs
+++ Utils/Common/Router.cs
@@ -22,8 +22,6 @@
         public static readonly RouterItem Register = new RouterItem() { Page = "/Views/Containers/Auth/Register.cshtml", Title = "Register", Link = "/auth/register" };
         public static readonly RouterItem LoginTutor = new RouterItem() { Page = "/Views/Containers/Auth/LoginTutor.cshtml", Title = "Login", Link = "/auth/login/tutor" };
         public static readonly RouterItem Login = new RouterItem() { Page = "/Views/Containers/Auth/Login.cshtml", Title = "Login", Link = "/auth/login" };
-
-
         public static readonly RouterItem Home = new RouterItem() { Page = "/Views/Containers/Home.cshtml", Title = "Home", Link = "/" };
     }
 }
