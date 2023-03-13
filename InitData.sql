d5953786ee75c2bcdd78d9eb79abcf8f0f3e42b4
TaiTang <tangtaidvlp@gmail.com>
2023-03-14 00:05:37 +0700

    [Tai] Add seed data script

---
+++ InitData.sql
@@ -0,0 +1,42 @@
+use Tutoree;
+
+-- Init Majors
+INSERT INTO tblMajor (MajorId, MajorName) VALUES ('1', 'Software Engineer');
+INSERT INTO tblMajor (MajorId, MajorName) VALUES ('2', 'Graphic Design');
+INSERT INTO tblMajor (MajorId, MajorName) VALUES ('3', 'Artificial Intelligent');
+INSERT INTO tblMajor (MajorId, MajorName) VALUES ('4', 'International Business');
+INSERT INTO tblMajor (MajorId, MajorName) VALUES ('5', 'English Specialist');
+
+-- Init School Year
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('1','1'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('2','2'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('3','3'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('4','4'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('5','5'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('6','6'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('7','7'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('8','8'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('9','9'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('10','10'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('11','11'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('12','12'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('13','13'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('14','14'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('15','15'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('16','16'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('17','17'); 
+INSERT INTO tblSchoolYear(SchoolYearId, SchoolYearName) VALUES ('18','18'); 
+
+-- Inits Locations
+INSERT INTO tblLocation VALUES ('1', 'FPT University - HCM');
+INSERT INTO tblLocation VALUES ('2', 'FPT University - Can Tho');
+INSERT INTO tblLocation VALUES ('3', 'FPT University - Da Nang');
+INSERT INTO tblLocation VALUES ('4', 'FPT University - Ha Noi');
+INSERT INTO tblLocation VALUES ('5', 'FPT Greenwich - HCM');
+INSERT INTO tblLocation VALUES ('6', 'FPT Polytechnic - HCM');
+INSERT INTO tblLocation VALUES ('7', 'FPT Polytechnic - Ha Noi');
+INSERT INTO tblLocation VALUES ('8', 'FPT Polytechnic - Da Nang');
+INSERT INTO tblLocation VALUES ('9', 'FPT Polytechnic - Quy Nhon');
+INSERT INTO tblLocation VALUES ('10', 'FPT Polytechnic - Tay Nguyen');
+INSERT INTO tblLocation VALUES ('11', 'FPT Polytechnic - Dong Nai');
+
